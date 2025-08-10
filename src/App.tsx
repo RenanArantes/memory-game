f5b0c4724088779b24b5101f0de729f0331871ad
Renan Arantes <malevo18@gmail.com>
2025-08-09 22:54:39 -0300

    chore: import test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Hello } from "@/components/Hello";
+
 function App() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-blue-200">
-      <h1 className="text-3xl font-bold text-pink underline">Hello world!</h1>
+      <Hello />
     </div>
   );
 }
